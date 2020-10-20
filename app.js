d25fd2b118d2110bdcdd0492d8b41e2225febeda
baberparweez <baberparweez@gmail.com>
2020-10-20 21:40:17 +0100

    add class, gerenate hex function and random colours function

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-// Globals selections and variables
+class Coloors {
+    constructor() {
+        // Globals selections and variables
+        this.colourDivs = document.querySelectorAll(".colour");
+        this.generateBtn = document.querySelector(".generate");
+        this.sliders = document.querySelectorAll('input[type="range"]');
+        this.currentHexes = document.querySelectorAll(".colour h2");
+        this.initalColours;
+    }
 
-// Functions
+    // Colour Generator
+    generateHex() {
+        const letters = "0123456789ABCDEF";
+        let hash = "#";
+        for (let i = 0; i < 6; i++) {
+            hash += letters[Math.floor(Math.random() * 16)];
+        }
+        return hash;
+    }
+
+    randomColours() {
+        this.colourDivs.forEach((div, index) => {
+            const hexText = div.children[0];
+            const randomColour = this.generateHex();
+
+            // Add the colour to the background
+            div.style.backgroundColor = randomColour;
+
+            console.log(randomColour);
+        });
+    }
+}
+
+const coloors = new Coloors();
+
+let output = coloors.randomColours();
+console.log(output);
